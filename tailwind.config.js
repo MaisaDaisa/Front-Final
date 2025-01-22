a9793d6331387d686c7792c7dae384ec5b438fe9
Nick <maisadzenika@gmail.com>
2025-01-22 20:17:44 +0400

    mobile media

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				searchGray: "#F0F0F0",
+				lightGray: "#F0F0F0",
+				creamyGray: "#F0EEED",
+			},
+			borderRadius: {
+				default: "20px",
+			},
+			gridTemplateColumns: {
+				dressStyles: "minmax(0, 1fr) minmax(0, 1.7fr)",
+				dressStylesReverse: "minmax(0, 1.7fr) minmax(0, 1fr)",
+			},
+			height: {
+				dressStyleHeight: "289px",
+			},
+			borderWidth: {
+				1: "1px",
 			},
 		},
 	},
